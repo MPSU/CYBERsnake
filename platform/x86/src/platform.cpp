0c4eae8838188456b27d937ae54aa1837558feae
Andrei Solodovnikov <voultboy@yandex.ru>
2025-04-21 18:06:03 +0300

    Finalize x86 platform

--- platform/x86/src/platform.cpp
+++ platform/x86/src/platform.cpp
@@ -12,12 +12,11 @@ details.
 #include "platform.h"
 
 std::atomic<bool> active;
-
-// std::vector<std::vector<uint8_t>> char_map(HEIGHT, std::vector<uint8_t>(WIDTH));
+std::atomic<uint8_t> cur_key = 0;
 uint8_t char_map[HEIGHT][WIDTH];
+
 std::default_random_engine rng;
 std::uniform_int_distribution<int> uniform_dist(WIDTH, WIDTH *(HEIGHT - 1));
-uint8_t cur_key;
 
 void timer_interrupt()
 {
@@ -26,51 +25,72 @@ void timer_interrupt()
   {
     game_cycle();
     redraw_frame();
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
   }
 }
 
 void blocking_input()
 {
-  while(true)
+  while (active)
   {
-    cur_key = getch();
+    if (_kbhit())
+    {
+      cur_key = _getch();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
+}
+
+void config_periph()
+{
+  for (size_t i = 0; i < HEIGHT; ++i)
+    for (size_t j = 0; j < WIDTH; ++j)
+      char_map[i][j] = SPACE_CHAR;
+
+  std::thread(timer_interrupt).detach();
+  std::thread(blocking_input).detach();
 }
 
 void redraw_frame()
 {
   // Clear terminal with special sequence
   // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
-  std::cout << "\x1B[2J\x1B[H";
+  std::string frame;
+  frame.reserve(WIDTH * HEIGHT + HEIGHT + 10);
+
+  frame += "\x1B[2J\x1B[H"; // clear + home
   for (size_t i = 0; i < HEIGHT; i++)
   {
-    for (size_t j = 0; j < WIDTH;  j++)
+    for (size_t j = 0; j < WIDTH; j++)
     {
-      std::cout << char_map[i][j];
+      frame += char_map[i][j];
     }
-    std::cout << std::endl;
+    frame += '\n';
   }
-}
 
-void config_periph()
-{
-  std::thread t1;
-  std::thread t2;
-  t1 = std::thread(timer_interrupt);
-  t2 = std::thread(blocking_input);
-  t1.join();
-  t2.join();
+  std::cout << frame;
+  std::cout.flush();
 }
 
 void print_symbol(size_t coord, uint8_t symbol)
 {
-  char_map[coord / WIDTH][coord % WIDTH] = symbol;
+  size_t row = coord / WIDTH;
+  size_t col = coord % WIDTH;
+  if (row < HEIGHT && col < WIDTH)
+  {
+    char_map[row][col] = symbol;
+  }
 }
 
 uint8_t get_symbol(size_t coord)
 {
-  return char_map[coord / WIDTH][coord % WIDTH];
+  size_t row = coord / WIDTH;
+  size_t col = coord % WIDTH;
+  if (row < HEIGHT && col < WIDTH)
+  {
+    return char_map[row][col];
+  }
+  return SPACE_CHAR;
 }
 
 bool get_key(uint8_t &key)
@@ -81,10 +101,7 @@ bool get_key(uint8_t &key)
     cur_key = 0;
     return true;
   }
-  else
-  {
-    return false;
-  }
+  return false;
 }
 
 size_t get_random_value()
