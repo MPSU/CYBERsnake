ec10232890c2df8a697d603446d61cf8fde2db92
Andrei Solodovnikov <voultboy@yandex.ru>
2025-05-31 16:42:57 +0300

    Update PRNG for CYBERcobra platform

--- platform/CYBERcobra RV EDITION/src/platform.cpp
+++ platform/CYBERcobra RV EDITION/src/platform.cpp
@@ -20,7 +20,7 @@ volatile VGA_HANDLE vga = {
 volatile TIMER_HANDLE *timer_ptr = CAST(struct TIMER_HANDLE *, 0x08000000);
 volatile PS2_HANDLE *ps2_ptr = CAST(struct PS2_HANDLE *, 0x03000000);
 extern Snake snake;
-
+size_t rng;
 void config_periph()
 {
   config_timer();
@@ -57,16 +57,21 @@ bool get_key(uint8_t &key)
 /*
   Actually, there is simple PRNG, known as minstd_rand0 defined in <random>
   but it seems to be too hard to work with this MCU.
-  So we need to get entropy from user input i.e. time when player reach snake
-  and dir of the snake at that moment. Since every game frame is happen at fixed
-  discretes of time, there is no much entropy in low bits of system timer.
-  We mix to entropy sources (system_counter and snake dir) and result value mask
-  with low 11 bits, because there is 2184 free coordinates at map 80x30
+  So we need to get entropy from user input i.e. time when player reach snake.
+  Since every game frame is happen at fixed discretes of time, there is no much
+  entropy in low bits of system timer.
+  We mix to entropy source with simple xorshift PRNG, which scaled down to 11
+  bits, because there is 2184 free coordinates at map 80x30
   (80*30-80*2-30*2+4) and we need to get value in that range.
 */
 size_t get_random_value()
 {
-  return ((timer_ptr->system_counter >> 20) ^ snake.dir) & 0x7FF;
+  rng ^= (timer_ptr->system_counter >> 20);
+  rng ^= rng << 5;
+  rng ^= rng >> 7;
+  rng ^= rng << 3;
+  rng &= 0x7FF; // Ограничиваем до 11 бит (0..2047)
+  return 81 + rng;
 }
 
 /*
@@ -79,6 +84,11 @@ size_t get_tick_number()
 
 size_t seed_rng(size_t seed)
 {
+  if(seed == 0)
+  {
+    seed = get_tick_number();
+  }
+  rng = seed;
   return seed;
 }
 
